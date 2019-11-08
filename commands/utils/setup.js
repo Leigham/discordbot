ea310c970d278785a29c3ef5bf3b44a76d02b585
itsLeigham <itsleigham@gmail.com>
2019-11-08 12:19:13 +0000

    made a start on the setup process

--- commands/utils/setup.js
+++ commands/utils/setup.js
@@ -1,4 +1,5 @@
 const fse = require('fs-extra')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = {
   name: 'setup',
@@ -9,18 +10,16 @@ module.exports = {
     if (args.length === 0) {
       // -- check if the setup has already been started.
       fse.pathExists(`./data/${message.guild.id}/config.json`, async (err, exists) => {
-        if (err) {
-          console.error(err)
-        };
+        if (err) return console.error
 
         if (exists) {
           await message.reply('It seems like the setup has already been started on this server, please type `!setup continue` to continue the setup!')
             .then(msg => setTimeout(() => msg.delete(), 5000))
             .catch(console.error)
           return
-        }
+        };
 
-        await message.reply('Please wait while I get things ready')
+        const tmp = await message.reply('Please wait while I get things ready')
 
         const tmpConfig = {
           id: message.guild.id,
@@ -35,6 +34,17 @@ module.exports = {
           }
           console.log('File saved successfully!')
         })
+
+        const embed = new MessageEmbed()
+          .setColor(0xff00ff)
+          .addField('Server Name', message.guild.name)
+          .addField('Server ID', message.guild.id)
+          .addField('Member Count', message.guild.members.size)
+          .addField('Admin Role', 'admin') // -- Default admin role.. this is not a physical role in the discord, but a `virtually created role`
+          .setTimestamp()
+
+        tmp.delete()
+        await message.channel.send(embed)
       })
     } else {
       if (!setupSteps.includes(args[0])) {
